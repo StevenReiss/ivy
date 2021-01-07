8fa4f97603dade927df04830d7d4ca0c7c615022
Steven Reiss <spr@cs.brown.edu>
2021-01-07 10:44:39 -0500

    Add missing interface file.

---
+++ javasrc/edu/brown/cs/ivy/jcomp/JcompExtendedSource1.java
@@ -0,0 +1,55 @@
+/********************************************************************************/
+/*                                                                              */
+/*              JcompExtendedSource1.java                                       */
+/*                                                                              */
+/*      Extended source with local AST nodes based on parameter                 */
+/*                                                                              */
+/********************************************************************************/
+/*      Copyright 2013 Brown University -- Steven P. Reiss                    */
+/*********************************************************************************
+ *  Copyright 2013, Brown University, Providence, RI.                            *
+ *                                                                               *
+ *                        All Rights Reserved                                    *
+ *                                                                               *
+ *  Permission to use, copy, modify, and distribute this software and its        *
+ *  documentation for any purpose other than its incorporation into a            *
+ *  commercial product is hereby granted without fee, provided that the          *
+ *  above copyright notice appear in all copies and that both that               *
+ *  copyright notice and this permission notice appear in supporting             *
+ *  documentation, and that the name of Brown University not be used in          *
+ *  advertising or publicity pertaining to distribution of the software          *
+ *  without specific, written prior permission.                                  *
+ *                                                                               *
+ *  BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS                *
+ *  SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND            *
+ *  FITNESS FOR ANY PARTICULAR PURPOSE.  IN NO EVENT SHALL BROWN UNIVERSITY      *
+ *  BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY          *
+ *  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,              *
+ *  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS               *
+ *  ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE          *
+ *  OF THIS SOFTWARE.                                                            *
+ *                                                                               *
+ ********************************************************************************/
+
+
+
+package edu.brown.cs.ivy.jcomp;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+
+public interface JcompExtendedSource1 extends JcompSource
+{
+
+
+ASTNode getAstRootNode(Object param);
+
+    
+
+
+}       // end of interface JcompExtendedSource1
+
+
+
+
+/* end of JcompExtendedSource1.java */
+
