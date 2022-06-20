decbbf9a46d67e29aefe76b527c8a41a0b40e63b
StevenReiss <spr@cs.brown.edu>
2022-06-20 08:04:20 -0400

    Fix warnings?

--- javasrc/edu/brown/cs/ivy/jannot/tree/JannotTreeJCCase.java
+++ javasrc/edu/brown/cs/ivy/jannot/tree/JannotTreeJCCase.java
@@ -104,7 +104,7 @@ JannotTreeJCCase(SwitchCase n)
 /*										*/
 /********************************************************************************/
 
-@SuppressWarnings("all")
+@SuppressWarnings({"All","deprecation"})
 @Override public JannotTreeJCExpression getExpression()
 {
    for (Object o : getCase().expressions()) {
